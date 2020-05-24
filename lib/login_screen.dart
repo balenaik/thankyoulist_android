ee08c7c29b35f0547b32cbd733f09fafca7e67db
Aika Yamada <balenaik@gmail.com>
2020-05-25 00:30:50 +0900

    Update login screen with an icon

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 import 'package:thankyoulist/main_screen.dart';
+import 'package:thankyoulist/app_colors.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key key, this.title}) : super(key: key);
@@ -56,24 +57,53 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
+      body: Container(
+        color: AppColors.backgroundYellow,
+        child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(image: AssetImage("assets/ic_login_screen.png"), height: 250.0),
+                _signInButton(),
+              ],
+            )
+        ),
       ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                child: Text('Sign in Google'),
-                onPressed: () {
-                  _handleSignIn()
-                      .then((FirebaseUser user) =>
-                      transitionNextPage(user)
-                  )
-                      .catchError((e) => print(e));
-                },
+    );
+  }
+
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () {
+        _handleSignIn()
+            .then((FirebaseUser user) =>
+            transitionNextPage(user)
+        )
+            .catchError((e) => print(e));
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
               ),
-            ]
+            )
+          ],
         ),
       ),
     );
