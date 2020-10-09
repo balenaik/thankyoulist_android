182a180910b1a64a88255306ed182b8911d1863f
Aika Yamada <balenaik@gmail.com>
2020-10-10 01:04:51 +0900

    Extract AddThankYouTextField

--- lib/views/screens/add_thankyou/add_thankyou_screen.dart
+++ lib/views/screens/add_thankyou/add_thankyou_screen.dart
@@ -4,41 +4,48 @@ class AddThankYouScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Add Thank You'),
-      ),
-      backgroundColor: Colors.grey[200],
-      body: ListView(
-        children: <Widget>[
-          Container(
-              margin: EdgeInsets.symmetric(
-                  vertical: 24.0,
-                  horizontal: 24.0
-              ),
-              child: TextField(
-                style: TextStyle(
-                    fontFamily: 'Nunito',
-                    fontSize: 17
-                ),
-              minLines: 4,
-              maxLines: null,
-              decoration: InputDecoration(
-                hintText: 'What are you thankful for?',
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide(color: Theme.of(context).unselectedWidgetColor, width: 2.0)
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide(color: Theme.of(context).primaryColor, width: 2.0)
-                ),
-                filled: true,
-                fillColor: Colors.white
-              ),
-            )
-          )
-        ]
-      )
+        appBar: AppBar(
+          title: Text('Add Thank You'),
+        ),
+        backgroundColor: Colors.grey[200],
+        body: ListView(
+            children: <Widget>[
+              AddThankYouTextField()
+            ]
+        )
+    );
+  }
+}
+
+class AddThankYouTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.symmetric(
+            vertical: 24.0,
+            horizontal: 24.0
+        ),
+        child: TextField(
+          style: TextStyle(
+              fontSize: 17
+          ),
+          minLines: 4,
+          maxLines: null,
+          decoration: InputDecoration(
+              hintText: 'What are you thankful for?',
+              enabledBorder: _outlineBorder(Theme.of(context).unselectedWidgetColor),
+              focusedBorder: _outlineBorder(Theme.of(context).primaryColor),
+              filled: true,
+              fillColor: Colors.white
+          ),
+        )
+    );
+  }
+  
+  OutlineInputBorder _outlineBorder(Color color) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10.0),
+        borderSide: BorderSide(color: color, width: 2.0)
     );
   }
 }
