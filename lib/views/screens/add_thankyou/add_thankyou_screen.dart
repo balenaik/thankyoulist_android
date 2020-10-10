04e1ea971ccddb16ac5b1f2f8a5487236736eb76
Aika Yamada <balenaik@gmail.com>
2020-10-10 20:10:00 +0900

    Add date container

--- lib/views/screens/add_thankyou/add_thankyou_screen.dart
+++ lib/views/screens/add_thankyou/add_thankyou_screen.dart
@@ -10,7 +10,8 @@ class AddThankYouScreen extends StatelessWidget {
         backgroundColor: Colors.grey[200],
         body: ListView(
             children: <Widget>[
-              AddThankYouTextField()
+              AddThankYouTextField(),
+              AddThankYouDatePicker()
             ]
         )
     );
@@ -21,14 +22,14 @@ class AddThankYouTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.symmetric(
-            vertical: 24.0,
-            horizontal: 24.0
+        margin: EdgeInsets.only(
+            left: 24.0,
+            right: 24.0,
+            top: 24.0,
+            bottom: 12.0
         ),
         child: TextField(
-          style: TextStyle(
-              fontSize: 17
-          ),
+          style: TextStyle(fontSize: 17),
           minLines: 4,
           maxLines: null,
           decoration: InputDecoration(
@@ -41,11 +42,39 @@ class AddThankYouTextField extends StatelessWidget {
         )
     );
   }
-  
+
   OutlineInputBorder _outlineBorder(Color color) {
     return OutlineInputBorder(
         borderRadius: BorderRadius.circular(10.0),
         borderSide: BorderSide(color: color, width: 2.0)
+    );
+  }
+}
+
+class AddThankYouDatePicker extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Theme.of(context).unselectedWidgetColor,
+                spreadRadius: 2.0),
+          ],
+        ),
+        child: Container(
+            margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 12.0),
+            child: Row(
+                children: <Widget>[
+                  Expanded(child: Text('Date')),
+                  Expanded(child: Text('2020/11/21', textAlign: TextAlign.right))
+                ]
+            ),
+            height: 50
+        )
     );
   }
 }
