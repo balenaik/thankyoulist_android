92c0acaa457789fdf21ef742bc277876c7e0288a
Aika Yamada <balenaik@gmail.com>
2021-04-29 11:11:11 +0900

    Modal edit thank you screen when the cell is tapped

--- lib/repositories/thankyou_repiository.dart
+++ lib/repositories/thankyou_repiository.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:thankyoulist/models/thankyou_create_model.dart';
+import 'package:thankyoulist/models/thankyou_model.dart';
 
 const USERS_COLLECTION = 'users';
 const THANKYOULIST_COLLECTION = 'thankYouList';
 
 abstract class ThankYouRepository {
+  Future<ThankYouModel> fetchThankYou(String userId, String id);
   Future<void> createThankYou(String userId, ThankYouCreateModel thankYouCreate);
 }
 
@@ -14,6 +17,17 @@ class ThankYouRepositoryImpl implements ThankYouRepository {
       : assert(firestore != null);
 
   final Firestore firestore;
+
+  @override
+  Future<ThankYouModel> fetchThankYou(String userId, String id) async {
+    final thankYouDocument = await firestore
+        .collection(USERS_COLLECTION)
+        .document(userId)
+        .collection(THANKYOULIST_COLLECTION)
+        .document(id)
+        .get();
+    return ThankYouModel.fromJson(json: thankYouDocument.data, documentId: id, userId: userId);
+  }
 
   @override
   Future<void> createThankYou(String userId, ThankYouCreateModel thankYouCreate) async {
---
+++ lib/viewmodels/edit_thankyou_view_model.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:thankyoulist/models/thankyou_model.dart';
+import 'package:thankyoulist/models/thankyou_update_model.dart';
+import 'package:thankyoulist/repositories/auth_repository.dart';
+import 'package:thankyoulist/repositories/thankyou_repiository.dart';
+import 'package:thankyoulist/status.dart';
+
+class EditThankYouStatus extends Status {
+  EditThankYouStatus(String value) : super(value);
+
+  static const editThankYouEditing = Status('EDIT_THANKYOU_EDITING');
+  static const editThankYouSuccess = Status('EDIT_THANKYOU_SUCCESS');
+  static const editThankYouFailed = Status('EDIT_THANKYOU_FAILED');
+}
+
+class EditThankYouViewModel with ChangeNotifier {
+  String _inputValue;
+  DateTime _selectedDate;
+  Status _status = Status.none;
+  ThankYouModel _editingThankYou;
+
+  String get inputValue => _inputValue;
+  DateTime get selectedDate => _selectedDate;
+  Status get status => _status;
+  ThankYouModel get editingThankYou => _editingThankYou;
+
+  final ThankYouRepository thankYouRepository;
+  final AuthRepository authRepository;
+
+  final String editingThankYouId;
+
+  EditThankYouViewModel(this.thankYouRepository, this.authRepository, this.editingThankYouId) {
+    _loadEditingThankYou();
+  }
+
+  void updateInputValue(String value) {
+    _inputValue = value;
+  }
+
+  void updateSelectedDate(DateTime selectedDate) {
+    _selectedDate = selectedDate;
+    notifyListeners();
+  }
+
+  Future<void> createThankYou() async {  }
+
+  void _loadEditingThankYou() async {
+    final userId = await authRepository.getUserId();
+    _editingThankYou = await thankYouRepository.fetchThankYou(userId, editingThankYouId);
+    _inputValue = _editingThankYou.encryptedValue;
+    _selectedDate = _utcDateTime(_editingThankYou.date);
+    notifyListeners();
+  }
+
+  DateTime _utcDateTime(DateTime datetime) {
+    return DateTime.utc(datetime.year, datetime.month, datetime.day);
+  }
+}
