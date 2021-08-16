d1dcb0b6e0ef14391927fae3e75f0b64657b61a1
Aika Yamada <balenaik@gmail.com>
2021-08-16 15:39:24 +0900

    Setup crashlytics initialization

---
+++ lib/supports/firebase_initializer.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+
+class FirebaseInitializer {
+  static Future<void> initializeFirebase() async {
+    await Firebase.initializeApp();
+    _initializeCrashlytics();
+  }
+
+  static void _initializeCrashlytics() {
+    if(kDebugMode){
+      FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+    } else {
+      FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+    }
+  }
+}
